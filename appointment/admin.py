aba63735c63ff513fad8dd72dc885fa6e4b5a5b0
H-M-Nizum <hmnizum1714032@gmail.com>
2024-01-11 00:24:49 +0600

    Appointment Model

--- appointment/admin.py
+++ appointment/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import AppiontmentModel
 # Register your models here.
+
+class AppiontmentAdmin(admin.ModelAdmin):
+    list_display = ['patient_name', 'doctor_name', 'appointment_status', 'appointment_type', 'symptop', 'time', 'cancel']
+
+    def patient_name(self, obj):
+        return obj.patient.user.first_name
+    def doctor_name(self, obj):
+        return obj.doctor.user.first_name
+
+admin.site.register(AppiontmentModel, AppiontmentAdmin)
