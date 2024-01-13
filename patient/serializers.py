97565736ce170d102367b115fd4acce644551dc5
H-M-Nizum <hmnizum1714032@gmail.com>
2024-01-13 20:26:08 +0600

    Send Registration email

--- patient/serializers.py
+++ patient/serializers.py
@@ -36,5 +36,6 @@ class PatientRegistrationSerializer(serializers.ModelSerializer):
         account = User(username=username, email=email, first_name=first_name, last_name=last_name)
         print(account)
         account.set_password(password)
+        account.is_active = False
         account.save()
         return account
--- patient/views.py
+++ patient/views.py
@@ -7,6 +7,14 @@ from . import serializers
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+
+# for sending email
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
 
 class PatientViewset(viewsets.ModelViewSet):
     queryset = models.PatientModel.objects.all()
@@ -23,5 +31,20 @@ class PatientRegisterViewsset(APIView):
             user = serializer.save()
             print(user)
 
-            return Response("Submisssion done")
+            token = default_token_generator.make_token(user)
+            print('token', token)
+
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            print(uid)
+
+            confirm_link = f"http://127.0.0.1:8000/patient/active/{uid}/{token}"
+
+            email_subject = 'Çonfirm your email'
+            email_body = render_to_string('confirm_email.html', {'confirm_link' : confirm_link})
+            
+            email = EmailMultiAlternatives(email_subject, '', to=[user.email])
+            email.attach_alternative(email_body, "text/html")
+            email.send()
+
+            return Response(" Check Your mail for confirmation")
         return Response(serializer.errors)
