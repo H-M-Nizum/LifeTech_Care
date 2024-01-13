7e44bc80624a0f2fcfdd9787d5bcc7177a87be81
H-M-Nizum <hmnizum1714032@gmail.com>
2024-01-13 19:40:40 +0600

    User Registration

--- patient/urls.py
+++ patient/urls.py
@@ -9,4 +9,5 @@ router.register('list', views.PatientViewset) # router ar antena
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', views.PatientRegisterViewsset.as_view(), name='register'),
 ]
--- patient/views.py
+++ patient/views.py
@@ -5,6 +5,23 @@ from rest_framework import viewsets
 from . import models
 from . import serializers
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 class PatientViewset(viewsets.ModelViewSet):
     queryset = models.PatientModel.objects.all()
     serializer_class = serializers.PatientSerializers
+
+
+class PatientRegisterViewsset(APIView):
+    serializer_class = serializers.PatientRegistrationSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            print(user)
+
+            return Response("Submisssion done")
+        return Response(serializer.errors)
